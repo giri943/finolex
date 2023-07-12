f320f8d800135d52d70be49c4633ac8c5e4383fe
Girish <girish.soman@schbang.com>
2023-07-12 23:26:06 +0530

    fix:second  tab toogle issue fixed in js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 
 const tabs = document.querySelectorAll('.tab-btn');
 const allContent = document.querySelectorAll('.content')
-const innerTabs = document.querySelectorAll('.inner-btn')
-const innerContentBenefit = document.querySelector('.benefits');
-const innerContentApp = document.querySelector('.application')
-console.log(innerContentBenefit);
+
 tabs.forEach((tab, index) =>{
     tab.addEventListener('click', ()=>{
         tabs.forEach(tab=>{
@@ -17,21 +14,26 @@ tabs.forEach((tab, index) =>{
     })
 })
 
-innerTabs.forEach((tab, index)=>{
-    tab.addEventListener('click', ()=>{
-        innerTabs.forEach(tab=>{
-            tab.classList.remove('active')
-        })
-        tab.classList.add('active')
-        console.log(tab);
-        console.log("Index",index );
-        if (tab.innerText === 'Benefits') {
-            innerContentBenefit.classList.add('active');
-            innerContentApp.classList.remove('active');
-          } else if (tab.innerText === 'Applications') {
-            innerContentBenefit.classList.remove('active');
-            innerContentApp.classList.add('active');
-          }
-    })
-    
-})
+allContent.forEach((content) => {
+  const innerTabs = content.querySelectorAll('.inner-btn');
+  const innerContentBenefit = content.querySelector('.benefits');
+  const innerContentApp = content.querySelector('.application');
+
+  innerTabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      innerTabs.forEach((tab) => {
+        tab.classList.remove('active');
+      });
+      tab.classList.add('active');
+
+      if (tab.innerText === 'Benefits') {
+        innerContentBenefit.classList.add('active');
+        innerContentApp.classList.remove('active');
+      } else if (tab.innerText === 'Applications') {
+        innerContentBenefit.classList.remove('active');
+        innerContentApp.classList.add('active');
+      }
+    });
+  });
+});
+
